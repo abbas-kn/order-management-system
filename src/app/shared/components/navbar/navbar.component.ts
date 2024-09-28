6ac803ca1bcc220e867d39e92cc24e24cb97629e
ahmadtahir-bit <ahmad.tahir@ebitlogix.com>
2024-09-28 19:50:48 +0500

    UI updated for login, add product and create order screen

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,5 +11,11 @@ import { CommonModule } from '@angular/common';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  constructor(private router: Router, private authService:AuthService) { }
+
+  logOut(){
+    this.authService.logOut();
+  }
 
 }
